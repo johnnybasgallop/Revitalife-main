b559c9c54d109214d16abacead8b571856dd7119
johnnybasgallop <johnnybasgallopwork@outlook.com>
2025-10-02 19:28:56 +0100

    fixed linting errors

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,6 +2,6 @@ export { BuySection } from "./BuySection";
 export { FAQSection } from "./FAQSection";
 export { HeroSection } from "./HeroSection";
 export { IngredientsSection } from "./IngredientsSection";
-export { NutritionistPage } from "./NutritionistPage.tsx";
+export { NutritionistPage } from "./NutritionistPage";
 export { OurApp } from "./OurApp";
 export { TestimonialsSection } from "./TestimonialsSection";
